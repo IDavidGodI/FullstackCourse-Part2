1ca8744b956d968393ec7cca49ff5c45cbda02f1
Carlos Romero <c6099020@gmail.com>
2023-12-01 22:03:33 -0500

    Exercises 2&3: Computing total with reduce

--- courseinfo/src/components/Course.jsx
+++ courseinfo/src/components/Course.jsx
@@ -1,5 +1,6 @@
 import Header from "./Header"
 import Content from "./Content"
+import Total from "./Total"
 
 const Course = ({course})=>{
   return (
--- courseinfo/src/components/Total.jsx
+++ courseinfo/src/components/Total.jsx
@@ -1,6 +1,7 @@
 const Total = ({parts})=>{
+  const total = parts.reduce((s,p)=> (s+p.exercises),0)
   return(
-    <p>Number of exercises {parts[0].exercises+parts[1].exercises+parts[2].exercises}</p>
+    <p>Number of exercises {total}</p>
   )
 }
 

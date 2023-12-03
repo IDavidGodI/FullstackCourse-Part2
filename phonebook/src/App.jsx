0a32a73aba8ab5ef2eab98b21f2436b96ddebb8a
Carlos Romero <c6099020@gmail.com>
2023-12-03 16:43:34 -0500

    Exercise 9: The phonebook, adding searching field

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -2,26 +2,31 @@ import { useState } from 'react'
 
 const Person = ({person})=>{
   return(
-    <p>{person.name} {person.phone}</p>
+    <p>{person.name} {person.number}</p>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '040-1234567' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456'},
+    { name: 'Ada Lovelace', number: '39-44-5323523'},
+    { name: 'Dan Abramov', number: '12-43-234345'},
+    { name: 'Mary Poppendieck', number: '39-23-6423122'}
+  ])
   const [newName, setNewName] = useState('')
-  const [newPhone, setNewPhone] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const handleNewName = (event)=> setNewName(event.target.value);
-  const handleNewPhone = (event)=> setNewPhone(event.target.value);
+  const handleNewNumber = (event)=> setNewNumber(event.target.value);
+  const handleFilter = (event)=> setFilter(event.target.value);
 
   const addContact = (event)=>{
     event.preventDefault()
     const trimmedName = newName.trim()
-    const trimmedPhone = newPhone.trim()
+    const trimmedNumber = newNumber.trim()
 
-    if (trimmedName.length===0 || trimmedPhone.length===0){
+    if (trimmedName.length===0 || trimmedNumber.length===0){
       alert("Fill the two fields")
       return
     }
@@ -30,26 +35,32 @@ const App = () => {
       alert(`${trimmedName} already exists in your contacts`)
       return
     }
-    if (persons.some((person)=>person.phone===trimmedPhone)){
-      alert(`The number ${trimmedPhone} already exists in your contacts`)
+    if (persons.some((person)=>person.number===trimmedNumber)){
+      alert(`The number ${trimmedNumber} already exists in your contacts`)
       return
     }
 
-    const newPerson = {name: trimmedName, phone: trimmedPhone}
+    const newPerson = {name: trimmedName, number: trimmedNumber}
     setPersons(persons.concat(newPerson))
     setNewName('')
-    setNewPhone('')
+    setNewNumber('')
   }
+
+  const peopleToShow = persons.filter(person => person.name.startsWith(filter) || person.number.startsWith(filter))
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input value={filter} onChange={handleFilter}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addContact}>
         <div>
           name: <input value={newName} onChange={handleNewName}/>
         </div>
         <div>
-          phone: <input value={newPhone} onChange={handleNewPhone}/>
+          Number: <input value={newNumber} onChange={handleNewNumber}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -57,8 +68,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {
-        persons.map(person=>{
-          console.log(person)
+        peopleToShow.map(person=>{
           return <Person key={person.name} person={person}/>
         })
       }

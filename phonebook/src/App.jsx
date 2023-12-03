3bc8793266126d87f4bd90747f87fb922ed8dce3
Carlos Romero <c6099020@gmail.com>
2023-12-03 16:25:51 -0500

    Exercise 8: The phonebook, allowing to add phone numbers

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -2,28 +2,43 @@ import { useState } from 'react'
 
 const Person = ({person})=>{
   return(
-    <p>{person.name}</p>
+    <p>{person.name} {person.phone}</p>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', phone: '040-1234567' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
 
   const handleNewName = (event)=> setNewName(event.target.value);
+  const handleNewPhone = (event)=> setNewPhone(event.target.value);
 
   const addContact = (event)=>{
     event.preventDefault()
     const trimmedName = newName.trim()
+    const trimmedPhone = newPhone.trim()
+
+    if (trimmedName.length===0 || trimmedPhone.length===0){
+      alert("Fill the two fields")
+      return
+    }
+
     if (persons.some((person)=>person.name===trimmedName)){
       alert(`${trimmedName} already exists in your contacts`)
       return
     }
-    const newPerson = {name: trimmedName}
+    if (persons.some((person)=>person.phone===trimmedPhone)){
+      alert(`The number ${trimmedPhone} already exists in your contacts`)
+      return
+    }
+
+    const newPerson = {name: trimmedName, phone: trimmedPhone}
     setPersons(persons.concat(newPerson))
     setNewName('')
+    setNewPhone('')
   }
 
   return (
@@ -32,6 +47,9 @@ const App = () => {
       <form onSubmit={addContact}>
         <div>
           name: <input value={newName} onChange={handleNewName}/>
+        </div>
+        <div>
+          phone: <input value={newPhone} onChange={handleNewPhone}/>
         </div>
         <div>
           <button type="submit">add</button>
